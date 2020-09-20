db7bfc9a465968927c3729671778bf25228ababd
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2020-09-20 17:44:06 +0530

    Assignment3-3

--- largeFileFromDir/largeSize.c
+++ largeFileFromDir/largeSize.c
@@ -1,7 +1,10 @@
+
 #include<stdio.h>
 #include<dirent.h>
 #include<fcntl.h>
 #include<unistd.h>//for lseek
+#include<string.h>
+#include<sys/stat.h> //for stat function
 int main(int argv,char *args[])
 {
     if(argv!=2)
@@ -13,9 +16,12 @@ int main(int argv,char *args[])
     int fd;
     int size;
     int maxsize=0;
-    char *file;
+    const char* file;
+    char* largefile;
+    struct stat ptr;
     struct dirent *de; //Pointer for directoery Entry
     DIR *dr=opendir(args[1]); ///it returns pointer
+    char* filename=args[1]+'/';
     if(dr==NULL)
     {
        printf("Unable to Open Directory");
@@ -23,16 +29,23 @@ int main(int argv,char *args[])
     }
     while((de=readdir(dr))!=NULL)
     {
-        fd=open(de->d_name,O_RDONLY);
-        size=lseek(fd,0,SEEK_END);
-        if(size>maxsize)
+        if(de->d_type==8)
         {
-            maxsize=size;
-            file=de->d_name;
+       // file=strcat(filename,de->d_name);
+       // printf("%s",file);
+       char* name=de->d_name;
+        if(stat((filename+name),&ptr)==0)
+        {
+            if(maxsize<ptr.st_size)
+            {
+                maxsize=ptr.st_size;
+                largefile=de->d_name;
+            }
+        }
         }
         //printf("%ld:%s:%c\n",de->d_ino,de->d_name,de->d_type);
     }
-    printf("File Having Lrgest Size %s:%d",file,maxsize);
+    printf("File Having Lrgest Size %s:%d",largefile,maxsize);
     closedir(dr);
 
     return 0;
