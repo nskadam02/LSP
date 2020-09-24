34be997c5f652d6f3e8eca9dd544c66c700d59fa
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2020-09-24 13:26:37 +0530

    Assignment3-6

--- CopyDirectory/copyDirectory.c
+++ CopyDirectory/copyDirectory.c
@@ -1,80 +1,52 @@
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// It Take The source and destination directory names from user and then copy all the files and directories 
+//from source to destination
 
-int main (int argc, char *argv[])
+#include<stdio.h>
+#include<dirent.h> //directory structure
+#include<stdlib.h>//dynamic memory allocation
+#include<sys/stat.h>// for stat
+#include<unistd.h> //for unlink lseek
+
+int main(int argv,char *args[])
 {
-   if (argc!=3)
-   {
-      printf("USAGE: programname source destination\n");
-      return 1;
-   }
+    if(argv!=3)
+    {
+        printf("Invalid Arguments\n");
+        printf("Run: Exename directoery_name");
+        return -1;
+    }
+    struct stat ptr;
+    int flag=0;
+    struct dirent *de; //Pointer for directoery Entry
+     char* n=(char*)malloc(256);
+      char* nwname=(char*)malloc(256);
+    DIR *dr=opendir(args[1]); ///it returns pointer
+    if(dr==NULL)
+    {
+       printf("Unable to Open Directory");
+       return -1;
+    }
+    while((de=readdir(dr))!=NULL)
+    {
+         //if(de->d_type==8)  //to consider only files not directories
+         //{
+           
+             sprintf(n,"%s/%s",args[1],de->d_name);  //concatenation to get the relative Path
+             sprintf(nwname,"%s/%s",args[2],de->d_name);
+             rename(de->d_name,nwname);
 
-   DIR *srcPtr;
-   
-   if (mkdir(argv[2],0777)<0)
-   {
-      printf("Unable to create directory %s (probably it already exists)\n",argv[2]);
-   }
-   srcPtr=opendir(argv[1]);
-   if (srcPtr==NULL)
-   {
-      printf("BAD OPENDIR %s\n",argv[1]);
-      return 1;
-   }
-   
-   struct dirent *dirPtr;
-   struct stat statbuff;
-   int fdSrc,fdDest;
-   char *path_copia;
-   unsigned int len_path_copia;
-   unsigned char c;
+        // }
+    }
 
-   chdir(argv[1]);
-   while ((dirPtr=readdir(srcPtr))!=NULL)
-   {
-      if (lstat(dirPtr->d_name,&statbuff)<0)
-      {
-         printf("BAD LSTAT\n");
-         return 1;
-      }
-      if (S_ISREG(statbuff.st_mode) &&
-         (statbuff.st_mode & S_IXUSR )) //IT's A REGULAR FILE WITH EXECUTION rights for the owner
-      {
-         //COPY----
-         len_path_copia=strlen(argv[2]);
-         len_path_copia+=1+strlen(dirPtr->d_name);
-         path_copia=(char *)malloc(len_path_copia*sizeof(char));
-         
-         strcpy(path_copia,argv[2]);
-         strcat(path_copia,dirPtr->d_name);
-         printf("Copying %s in %s\n",dirPtr->d_name,path_copia);
-         if ( (fdDest=open(path_copia,O_CREAT|O_TRUNC|O_WRONLY,(statbuff.st_mode &S_IRWXU)|(statbuff.st_mode &S_IRWXG)|(statbuff.st_mode &S_IRWXO) ) ) <0 )
-         {
-            printf("BAD OPEN %s\n",path_copia);
-            return 1;
-         }
-         if ( (fdSrc=open(dirPtr->d_name,O_RDONLY))<0 )
-         {
-            printf("BAD OPEN %s\n",dirPtr->d_name);
-            return 1;
-         }
-         while (read(fdSrc,&c,1)==1)
-         {
-            write(fdDest,&c,1);
-         }
-         close(fdSrc);
-         close(fdDest);
-         free(path_copia);
-      }
-      
-   }
-   chdir("..");
-   closedir (srcPtr);
-   return 0;
+    closedir(dr);
+
+    return 0;
 }
+/*
+struct dirent{
+ unsigned lond d_ino  //inode number;
+ char d_name[256]; //directroy name
+ long d_off //
+ unsigned short d_rclean;//
+ unsigned char d_type; //
+}*/
